5128c45e4152bc91500701c51fc748ffd556644e
Roy Ratcliffe <royratcliffe@me.com>
2020-01-21 08:25:33 +0000

    Document url, get

--- prolog/couchdb/document.pl
+++ prolog/couchdb/document.pl
@@ -4,3 +4,9 @@
 
 Document.rev(Rev) := couchdb_revision{document:Document, rev:Rev_} :-
     atom_string(Rev_, Rev).
+
+Document.url() := URL :-
+    atomic_list_concat([Document.database.db, Document.id], /, URL).
+
+Document.get() := Doc :-
+    Document.database.client.get_url(Document.url()) = Doc.
